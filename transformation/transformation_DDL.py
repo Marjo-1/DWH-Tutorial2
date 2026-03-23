ec054077d0dd980a45877996daf3aea51f3cba18
marnix heutz <m.heutz@student.maastrichtuniversity.nl>
2026-03-23 11:44:31 +0100

    Update transformation_DDL.py

--- transformation/transformation_DDL.py
+++ transformation/transformation_DDL.py
@@ -7,7 +7,7 @@ conn = pyodbc.connect(
     "PORT=5432;"
     "DATABASE=postgres;"
     "UID=postgres;"
-    "PWD=7002anuN;"
+    "PWD=*****;"
 )
 conn.autocommit = True
 cursor = conn.cursor()
@@ -31,7 +31,7 @@ conn = pyodbc.connect(
     "PORT=5432;"
     "DATABASE=dwh;"
     "UID=postgres;"
-    "PWD=7002anuN;"
+    "PWD=*****;"
 )
 conn.autocommit = True
 cursor = conn.cursor()
